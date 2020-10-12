afce9aef2c92d69f52bb5b5a40f3933914344f07
Martin Troiber <martin.troiber@gmail.com>
2020-10-12 23:17:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Translate Elm code to Haskell + Miso
+This project aims to aid with translating Elm code to Haskell + Miso. Currently it is based on simple regular expressions.
+In the future it should use the official Elm parser for this job.
---
+++ Translator.hs
@@ -0,0 +1,45 @@
+import System.Environment
+import Text.Regex
+
+-- only works on single lines!
+replacements :: [(Regex, String)]
+replacements =
+  [ (mkRegex "List\\.concat", "Prelude.concat")
+  , (mkRegex "List\\.map", "Prelude.map")
+  , (mkRegex "List\\.filterMap identity", "Maybe.mapMaybe id")
+  , (mkRegex "\\(\\((.*)\\) as config_", "(config_@\\1")
+  , (mkRegex "Config \\{ config_ \\|", "config_ {")
+  , (mkRegex "type ", "data ")
+  , (mkRegex "\\(Config config_\\)", "config_")
+  , (mkRegex "Html msg", "Miso.View msg")
+  , (mkRegex "Miso\\.Attibute\\.attribute ", "Miso.textProp ")
+  , (mkRegex "Miso\\.node ", "Miso.nodeHtml ")
+  , (mkRegex "Miso\\.input ", "Miso.input_ ")
+  , (mkRegex "Miso\\.div ", "Miso.div_ ")
+  , (mkRegex "Miso\\.td ", "Miso.td_ ")
+  , (mkRegex "Miso\\.th ", "Miso.th_ ")
+  , (mkRegex "Miso\\.tr ", "Miso.tr_ ")
+  , (mkRegex "Miso\\.thead ", "Miso.thead_ ")
+  , (mkRegex "Miso\\.tbody ", "Miso.tbody_ ")
+  , (mkRegex "class ", "Miso.class_ ")
+  , (mkRegex "Miso\\.table ", "Miso.table_ ")
+  , (mkRegex "Html\\.", "Miso.")
+  , (mkRegex "List \\(([^\\)]*)\\)", "[\\1]")
+  , (mkRegex " \\$: ", " :: ")
+  , (mkRegex " :: ", " : ")
+  , (mkRegex " : ", " $: ")
+  , (mkRegex "exposing", "")
+  ]
+
+translate :: [(Regex, String)] -> String -> String
+translate rs s = foldr (\(reg, rep) s -> subRegex reg s rep) s rs
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [inFile, outFile] -> do
+      inString <- readFile inFile
+      let outString = "{-# LANGUAGE OverloadedStrings #-}\n" ++ translate replacements inString
+      writeFile outFile outString
+    _ -> putStrLn "usage: Translator <inFile> <outFile>"
